4a1c0b60c9634ca25087aaf506eefc6133a80a4c
baffour01 <philipapea@gmail.com>
2024-05-31 19:05:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ total_sales_product['Sales']=round(total_sales_product['Sales'],2)
 total_sales_product=formatIndex(total_sales_product)
 
 st.dataframe(total_sales_product.head(10))
-st.dataframe(total_sales_product.tail(10))
 
-# fig=px.bar(total_sales_product,x= 'Product Name',y='Sales',orientation='h',color='Product Name')
-# st.plotly_chart(fig,use_container_width=True)
+fig=px.bar(total_sales_product,x= 'Product Name',y='Sales',orientation='h',color='Product Name')
+st.plotly_chart(fig,use_container_width=True)
